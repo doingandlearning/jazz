a033551862a194aabb93ffc597db6be674317ffd
Trisha Lim <hello@trishalim.com>
2025-01-10 16:19:30 +0000

    Add dashboard link to nav

--- homepage/homepage/components/nav.tsx
+++ homepage/homepage/components/nav.tsx
@@ -1,5 +1,7 @@
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { socials } from "@/lib/socials";
+import { Button } from "gcmp-design-system/src/app/components/atoms/Button";
+import { Icon } from "gcmp-design-system/src/app/components/atoms/Icon";
 import { JazzLogo } from "gcmp-design-system/src/app/components/atoms/logos/JazzLogo";
 import { Nav } from "gcmp-design-system/src/app/components/organisms/Nav";
 import { DocNav } from "./docs/nav";
@@ -7,14 +9,13 @@ import { DocNav } from "./docs/nav";
 export function JazzNav() {
   return (
     <Nav
-      mainLogo={<JazzLogo className="w-24" />}
+      mainLogo={<JazzLogo className="w-20 lg:w-24" />}
       themeToggle={ThemeToggle}
       items={[
         { title: "Jazz Cloud", href: "/cloud" },
         {
           title: "Docs",
           href: "/docs",
-          items: [],
         },
         {
           title: "Examples",
@@ -35,17 +36,23 @@ export function JazzNav() {
           newTab: true,
         },
         {
-          title: "Releases",
-          href: "https://github.com/garden-co/jazz/releases",
-          newTab: true,
-        },
-        {
           title: "Status",
           href: "/status",
         },
       ]}
       socials={socials}
       docNav={<DocNav className="block h-auto" />}
+      cta={
+        <Button
+          variant="primary"
+          href="https://cloud.jazz.tools"
+          size="sm"
+          className="ml-3"
+        >
+          Dashboard
+          <Icon name="chevronRight" size="xs" className="-mr-1" />
+        </Button>
+      }
     />
   );
 }
